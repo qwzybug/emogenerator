fabc902b4930c53c9ad2725257a084eb091a0262
Jonathan Wight <jwight@mac.com>
2009-05-04 22:18:08 -0400

    Updated to work with xcdatamodeld bundles.

--- emogenerator/emogenerator.py
+++ emogenerator/emogenerator.py
@@ -66,7 +66,7 @@ def main(args):
 		help='set the log level, 0 = no log, 10+ = level of logging')
 	parser.add_option('', '--logfile', action='callback', dest='logstream', type='string', default = sys.stderr, callback=store_open_file, callback_kwargs = {'mode':'w'}, metavar='LOG_FILE',
 		help='File to log messages to. If - or not provided then stdout is used.')
-	
+
 	(theOptions, theArguments) = parser.parse_args(args = args[1:])
 
 	logging.basicConfig(level = theOptions.loglevel, format = '%(message)s', stream = theOptions.logstream)
@@ -82,18 +82,24 @@ def main(args):
 def emogenerator(options, inArguments):
 	# If we don't have an input file lets try and find one in the cwd
 	if options.input == None:
-		options.input = glob.glob('*.xcdatamodel')[0]
+		files = glob.glob('*.xcdatamodel')
+		if files:
+			options.input = files[0]
+	if options.input == None:
+		files = glob.glob('*.xcdatamodeld')
+		if files:
+			options.input = files[0]
 	if options.input == None:
 		options.input = glob.glob('*.mom')[0]
-	
-	
+
+
 
 	# If we still don't have an input file we need to bail.
 	if not os.path.exists(options.input):
 		raise Exception('Input file doesnt exist at %s' % options.input)
 
 	options.input_type = os.path.splitext(options.input)[1][1:]
-	if options.input_type not in ['mom', 'xcdatamodel']:
+	if options.input_type not in ['mom', 'xcdatamodel', 'xcdatamodeld']:
 		raise Exception('Input file is not a .mom or a .xcdatamodel. Why are you trying to trick me?')
 
 	logging.info('Processing \'%s\'', options.input)
@@ -111,20 +117,19 @@ def emogenerator(options, inArguments):
 		CoreData.NSFloatAttributeType: dict(cocoaType = 'NSNumber', ctype = 'float', toCTypeConverter = 'floatValue', toCocoaTypeConverter = 'numberWithFloat'),
 		CoreData.NSBooleanAttributeType: dict(cocoaType = 'NSNumber', ctype = 'BOOL', toCTypeConverter = 'boolValue', toCocoaTypeConverter = 'numberWithBool'),
 		}
-	
-	if options.input_type == 'xcdatamodel':
+
+	if options.input_type in ['xcdatamodel', 'xcdatamodeld']:
 		logging.info('Using momc at \'%s\'', options.momcpath)
 		# Create a place to put the generated mom file
 		theTempDirectory = tempfile.mkdtemp()
 		theObjectModelPath = os.path.join(theTempDirectory, 'Output.mom')
-	
+
 		# Tell momc to compile our xcdatamodel into a managed object model
 		theResult = subprocess.call([options.momcpath, options.input, theObjectModelPath])
 		if theResult != 0:
 			raise Exception('momc failed with %d', theResult)
 	else:
 		theObjectModelPath = options.input
-
 
 	# No? Ok, let's fall back to the cwd
 	if options.template == None:
@@ -157,7 +162,7 @@ def emogenerator(options, inArguments):
 		if 'PBXCustomTemplateMacroDefinitions' in theXcodePrefs:
 			if 'ORGANIZATIONNAME' in theXcodePrefs['PBXCustomTemplateMacroDefinitions']:
 				theContext['organizationName'] = theXcodePrefs['PBXCustomTemplateMacroDefinitions']['ORGANIZATIONNAME']
-	
+
 	# Process each entity...
 	for theEntityDescription in theObjectModel.entities():
 		# Create a dictionary describing the entity, we'll be passing this to the genshi template.
@@ -169,11 +174,11 @@ def emogenerator(options, inArguments):
 			'properties': [],
 			'relatedEntityClassNames': [],
 			}
-			
+
 		if theEntityDict['className'] == 'NSManagedObject':
 			logging.info('Skipping entity \'%s\', no custom subclass specified.', theEntityDescription.name())
 			continue
-	
+
 		if theEntityDescription.superentity():
 			theEntityDict['superClassName'] = theEntityDescription.superentity().managedObjectClassName()
 
@@ -181,7 +186,7 @@ def emogenerator(options, inArguments):
 		for thePropertyDescription in theEntityDescription.properties():
 			if theEntityDescription != thePropertyDescription.entity():
 				continue
-	
+
 			# This dictionary describes the property and is appended to the entity dictionary we created earlier.
 			thePropertyDict = {
 				'property': thePropertyDescription,
@@ -189,14 +194,14 @@ def emogenerator(options, inArguments):
 				'type': thePropertyDescription.className(),
 				'CType': None,
 				}
-	
+
 			if thePropertyDescription.className() == 'NSAttributeDescription':
 				if thePropertyDescription.attributeType() not in theTypenamesByAttributeType:
 					logging.warning('Did not understand the property type: %d', thePropertyDescription.attributeType())
 					continue
-				
+
 				theTypenameByAttributeType = theTypenamesByAttributeType[thePropertyDescription.attributeType()]
-				
+
 				thePropertyDict['CocoaType'] = theTypenameByAttributeType['cocoaType']
 				if 'ctype' in theTypenameByAttributeType:
 					thePropertyDict['CType'] = theTypenameByAttributeType['ctype']
@@ -209,26 +214,26 @@ def emogenerator(options, inArguments):
 				theEntityDict['relatedEntityClassNames'].append(thePropertyDescription.destinationEntity().managedObjectClassName())
 			else:
 				continue
-	
+
 			theEntityDict['properties'].append(thePropertyDict)
-			
+
 		theEntityDict['attributes'] = [x for x in theEntityDict['properties'] if x['type'] == 'NSAttributeDescription']
 		theEntityDict['relationships'] = [x for x in theEntityDict['properties'] if x['type'] == 'NSRelationshipDescription']
-	
+
 		theTemplateNames = ['classname.h.genshi', 'classname.m.genshi']
 		for theTemplateName in theTemplateNames:
-	
+
 			theTemplate = theLoader.load(theTemplateName, cls=genshi.template.NewTextTemplate)
 
 			theContext['entity'] = theEntityDict
 
 			theStream = theTemplate.generate(**theContext)
 			theNewContent = theStream.render()
-			
+
 			theFilename = theEntityDescription.managedObjectClassName() + '.' + re.match(r'.+\.(.+)\.genshi', theTemplateName).group(1)
-		
+
 			theOutputPath = os.path.join(options.output, theFilename)
-			
+
 			if os.path.exists(theOutputPath) == False:
 				file(theOutputPath, 'w').write(theNewContent)
 			else:
@@ -239,7 +244,7 @@ def emogenerator(options, inArguments):
 					])
 				if theNewContent != theCurrentContent:
 					file(theOutputPath, 'w').write(theNewContent)
-		
+
 def merge(inTemplate, inOriginal, inDelimiters):
 	theComponents = []
 	for theDelimiter in inDelimiters:
@@ -251,7 +256,7 @@ def merge(inTemplate, inOriginal, inDelimiters):
 		theTemplateEnd = inTemplate.find(theDelimiter[1], theTemplateStart)
 		if not theTemplateEnd:
 			continue
-	
+
 		### Find delimited section in original
 		theOriginalStart = inOriginal.find(theDelimiter[0])
 		if not theOriginalStart:
@@ -260,11 +265,11 @@ def merge(inTemplate, inOriginal, inDelimiters):
 		theOriginalEnd = inOriginal.find(theDelimiter[1], theOriginalStart)
 		if not theOriginalEnd:
 			continue
-		
+
 		inOriginal = inOriginal[:theOriginalStart] + inTemplate[theTemplateStart:theTemplateEnd] + inOriginal[theOriginalEnd:]
-	
+
 	return inOriginal
-	
+
 # if __name__ == '__main__':
 # #	main(sys.argv)
 # 	os.chdir(os.path.expanduser('~/Desktop/emogenerator/test'))
