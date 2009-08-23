e2dfda6df1dfb541b9ca6f22758f0c431b1bf1a4
Jonathan Wight <jwight@mac.com>
2009-08-23 11:18:57 -0400

    Updated version #

--- emogenerator/emogenerator.py
+++ emogenerator/emogenerator.py
@@ -30,7 +30,7 @@ def main(args):
 		setattr(parser.values, option.dest, theFile)
 
 	theUsage = '''%prog [options] [INPUT]'''
-	theVersion = '%prog 0.1.10dev'
+	theVersion = '%prog 0.1.11dev'
 
 	# If no explicit path to momc is set ask 'which' for it.
 	theDefaultMomcPath = None
