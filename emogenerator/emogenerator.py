7fe8c8b5562e25176e670a425bfdc8bd23857969
Devin Chalmers <devin@luckysort.com>
2012-02-29 13:56:20 -0800

    Sanitize input directories with a trailing slash

--- emogenerator/emogenerator.py
+++ emogenerator/emogenerator.py
@@ -140,6 +140,10 @@ def emogenerator(options, inArguments):
 	if options.input == None:
 		raise Exception('Could not find a data model file.')
 
+	# Sanitize input directories with a trailing slash
+	if options.input[-1] == '/':
+		options.input = options.input[:-1]
+
 	# If we still don't have an input file we need to bail.
 	if not os.path.exists(options.input):
 		raise Exception('Input file doesnt exist at %s' % options.input)
@@ -147,8 +151,6 @@ def emogenerator(options, inArguments):
 	logger.info('Using \'%s\'' % options.input)
 
 	options.input_type = os.path.splitext(options.input)[1][1:]
-	if options.input_type[-1] == '/':
-		options.input_type = options.input_type[:-1]
 	if options.input_type not in ['mom', 'xcdatamodel', 'xcdatamodeld']:
 		raise Exception('Input file is not a .mom or a .xcdatamodel. Why are you trying to trick me?')
 
